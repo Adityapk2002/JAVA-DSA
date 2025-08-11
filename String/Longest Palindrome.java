929051ad88e43f6e87e1c835bfe5d6025eb7a0db
aditya khandade <adityakhandade2002@gmail.com>
2025-08-11 14:11:00 +0530

    Added new question

---
+++ String/Longest Palindrome.java
@@ -0,0 +1,28 @@
+package String;
+
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public int longestPalindrome(String s) {
+        Map<Character, Integer> freq = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+        int length = 0;
+        boolean oddFound = false;
+
+        for (int count : freq.values()) {
+            if (count % 2 == 0) {
+                length += count;
+            } else {
+                length += count - 1;
+                oddFound = true;
+            }
+        }
+        if (oddFound) {
+            length++;
+        }
+        return length;
+    }
+}
