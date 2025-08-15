e86f4f9a6eab44a3e409ba62cfd782b5ec01d78a
aditya khandade <adityakhandade2002@gmail.com>
2025-08-15 14:37:04 +0530

    1

---
+++ String/isPalindrome.java
@@ -0,0 +1,22 @@
+package String;
+
+public class isPalindrome {
+    public static boolean Palindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String a = "madam";
+        boolean ans = Palindrome(a);
+        System.out.println("The string is palindrome:" + ans);
+    }
+}
